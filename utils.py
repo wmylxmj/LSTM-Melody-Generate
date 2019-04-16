e04ae90e2d670bab4696e2a8abe168d9ad5378d7
wmy <1397322329@qq.com>
2019-04-16 09:25:54 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 22:18:35 2019
+
+@author: wmy
+"""
+
+import midi
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from tqdm import tqdm
+from parsers import MidiParser, SequenceParser
+
+class DataLoader(object):
+    '''Data Loader'''
+    
+    def __init__(self, name=None):
+        self.midi_parser = MidiParser()
+        self.sequence_parser = SequenceParser()
+        self.name = name
+        pass
+    
+    def load_songs(self, folder):
+        files = glob.glob('{}/*.mid*'.format(folder))
+        songs = []
+        for file in files:
+            song = self.midi_parser.parse(file)
+            song = np.array(song)
+            songs.append(song)
+            pass
+        return songs
+    
+    def load_dataset(self, folder, Tx, sample_interval=8):
+        songs = self.load_songs(folder)
+        X, Y = [], []
+        name = 0
+        for song in tqdm(songs):
+            name += 1
+            melody = self.sequence_parser.get_monosyllabic_melody(song)
+            melody = self.sequence_parser.monosyllabic_melody_to_matrix(melody)
+            for i in range(0, melody.shape[0]-Tx-1, sample_interval):
+                X.append(melody[i:i+Tx])
+                Y.append(melody[i+1:i+Tx+1])
+                pass
+            pass
+        X = np.array(X)
+        Y = np.array(Y)
+        return X, Y
+    
+    pass
